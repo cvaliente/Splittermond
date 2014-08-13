c6b2c144dfb2bf48ccf0fac4270828989bfb78cf
zaboron <csiebenkaes@gmail.com>
2014-08-13 12:44:47 +0200

    0

--- parsebewertungen.py
+++ parsebewertungen.py
@@ -44,12 +44,11 @@ class tableheaderrow(tablerow):
     def cellify(self, rowfield):
         return str('[td]' + bbbold(rowfield)+bbtt('   ')+'[/td]')
 
-def getProdukte():
+def getProdukte(bewertungsthreads):
     
     Produkt = namedtuple('Produkt','name id url Stimmen Durchschnitt')
     Produkte = []
     baseurl = "http://forum.splittermond.de/index.php?topic=%d.0"
-    bewertungsthreads = [1676,1418,2003]
     
     for threadid in bewertungsthreads:
         url=baseurl % threadid
@@ -59,18 +58,32 @@ def getProdukte():
         polls = soup.find('dl',{'class':'options'})
         options = polls.findAll('dt',{'class':'middletext'})
         votes = polls.findAll('span',{'class':'percentage'})
-        ergebnis = dict(zip([[int(s) for s in option.string.split() if s.isdigit(   )][0] for option in options], [int(vote.string.split(' ')[0]) for vote in votes]))
+        ergebnis = dict(zip([[int(s) for s in option.string.split() if s.isdigit()][0] for option in options], [int(vote.string.split(' ')[0]) for vote in votes]))
         try:
             average = str(round(sum(k*v for k,v in ergebnis.items()) / sum(v for k,v in ergebnis.items()),2))
-        except ValueError:
+        except ZeroDivisionError:
             average = 'No votes yet'
         Produkte.append(Produkt(Produktname, threadid, url, str(sum(v for k,v in ergebnis.items())), average))
     return Produkte
 
-print(bbtable([tableheaderrow(['Platz', 'Bewertung', 'Stimmen', 'Produkt'])] 
-            + [tablerow([index+1, element.Durchschnitt, element.Stimmen, bbcodeurl(element.url, element.name)]) 
-               for index, element in enumerate(sorted(getProdukte(), key=attrgetter('Durchschnitt')))]))
 
-print(bbtable([tableheaderrow(['Platz', 'Bewertung', 'Stimmen', 'Produkt'])] 
+def generateTable(bewertungsthreads):
+    return bbtable([tableheaderrow(['Platz', 'Bewertung', 'Stimmen', 'Produkt'])] 
             + [tablerow([index+1, element.Durchschnitt, element.Stimmen, bbcodeurl(element.url, element.name)]) 
-               for index, element in enumerate(sorted(getProdukte(), key=attrgetter('Durchschnitt')))]))
+               for index, element in enumerate(sorted(bewertungsthreads, key=attrgetter('Durchschnitt')))])
+
+abenteuer = [2003, 2097,2098, 2099, 2100, 2101]
+spielhilfen = [1676, 1418]
+
+bewertungen = getProdukte(abenteuer+spielhilfen)
+
+Abenteuerbewertungen = [Abenteuer for Abenteuer in bewertungen if Abenteuer.id in abenteuer]
+
+Spielhilfenbewertungen = [Spielhilfe for Spielhilfe in bewertungen if Spielhilfe.id in spielhilfen]
+print('Hier die Sammlung aller Produktbewertungsthreads, inklusive Durchschnittsbewertung und Ranking.')
+print('Ich versuche das Ganze auf aktuellen Stand zu halten. :)')
+print('Noch ist es nicht sonderlich spektakulär, einfach weil es noch nicht viele Produkte gibt. Aber ich hoffe das ändert sich mit der Zeit. :)')
+print(bbbold('\r\nSpielhilfen'))
+print(generateTable(Spielhilfenbewertungen))
+print(bbbold('\r\nAbenteuer'))
+print(generateTable(Abenteuerbewertungen))
--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,5 @@
-from random import randint
+bewertungsthreads = {1676:'Spielhilfe',1418:'Spielhilfe',2003:'Abenteuer', 2097:'Abenteuer',2098:'Abenteuer', 2099:'Abenteuer', 2100, 2101}
 
-print(randint(1, 10) + randint(1, 10))
-print(randint(1, 10) + randint(1, 10))
-print(randint(1, 10) + randint(1, 10) + randint(1, 10) + randint(1, 10) + randint(1, 10))
+
+spielhilfen = [1676,1418]
+
