4644d84f31429a17981b9229c9ffeef5830a88ec
zaboron <csiebenkaes@gmail.com>
2015-01-19 12:40:39 +0100

    s

--- parsebewertungen.py
+++ parsebewertungen.py
@@ -10,9 +10,9 @@ import time
 
 # Collection of Thread IDs in several categories
 Produktthreads = OrderedDict([
-('Spielhilfen' , [1676, 1418, 2361]),
+('Spielhilfen' , [1676, 1418, 2361, 2653]),
 ('Kaufabenteuer' ,[2003, 2097, 2360]),
-('Kostenlos verfügbare Abenteuer' , [2097,2098, 2099, 2100, 2101])
+('Kostenlos verfügbare Abenteuer' , [2097,2098, 2099, 2100, 2101, 2652, 2651])
 ])
 
 
